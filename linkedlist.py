aff8584f4eb1f6590f012d895f5acd9b7101a711
Sushank Reddy <120310925+sushankreddy04@users.noreply.github.com>
2024-03-20 20:29:39 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,18 +1,25 @@
+#here is the creation of linked list in adding from front and back and operation on linked list like finding 
+#middle element and adding all elements in linked list
+#for creating a node class
 class node:
     def __init__(self,x):
         self.data=x
         self.next=None
+#linking the nodes by link class
 class link:
     def __init__(self):
         self.head=None
+  
+#Adding in front of linked list
     def _add_front_(self,x):
         if(self.head==None):
             self.head=node(x)
         else:
-        
             temp=node(x)
             temp.next=self.head
             self.head=temp
+
+#Adding in back of linked list
     def _add_back_(self,x):
         if(self.head==None):
             self.head=node(x)
@@ -21,6 +28,8 @@ class link:
             while(temp.next!=None):
                 temp=temp.next
             temp.next=node(x)
+            
+#display the linked list
     def _display_(self):
         if(self.head==None):
             print("empty list")
@@ -29,6 +38,8 @@ class link:
             while(temp!=None):
                 print(temp.data,end="->")
                 temp=temp.next
+
+#To Add all elements present in a linked list
     def _addall_(self):
         if(self.head==None):
             print("empty list")
@@ -39,6 +50,8 @@ class link:
                     s=s+temp.data
                     temp=temp.next
             print(s)
+
+# To display middle element in linked list
     def _mid_(self):
         if(self.head==None):
             print('zero')
@@ -50,15 +63,7 @@ class link:
                 temps=temps.next
             print(temps.data)
 
-
-
-
-
-
-
-
-
-
+#declaring objects 
 l1=link()
 l1._add_back_(21)
 l1._add_back_(30)
